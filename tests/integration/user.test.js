eb9f227c1785f894ed7e8167e3e96581ef8d1a88
Manjunatha Reddy PM <mreddypm@gmail.com>
2023-02-24 14:01:57 +0530

    [Manjunatha]Aadded:test_for_note

--- tests/integration/user.test.js
+++ tests/integration/user.test.js
@@ -5,6 +5,8 @@ import mongoose from 'mongoose';
 import app from '../../src/index';
 
 describe('User APIs Test', () => {
+  var token;
+  
   before((done) => {
     const clearCollections = () => {
       for (const collection in mongoose.connection.collections) {
@@ -45,24 +47,149 @@ describe('User APIs Test', () => {
         });
     });
   });
-});
-
 
 describe('userLogin', () => {
   it('should return valid login', (done) => {
-  let login = {                                                    
+  let userLogin = {                                                    
     "email": "abcd@gmail.com",
     "password": "aA1"
   }
     request(app)
       .get('/api/v1/users/login')
-      .send(login)
-      .end((err, res) => {
-        
+      .send(userLogin)
+      .end((err, res) => { 
+        token = res.body.data.token;
+        console.log(token);
         expect(res.statusCode).to.be.equal(200);
         //expect(res.body.data).to.be.an('array');
-       
         done();
       });
   });
+});
+
+
+
+var id;
+describe('note', () => {
+  it('should return valid creation of note', (done) => {
+  let details = {                                                    
+    "title": "abcd",
+    "description": "aA1"
+  }
+    request(app)
+      .post('/api/v1/notes/')
+      .set('authorization', `bearer ${token}`)
+      .send(details)
+      .end((err, res) => {
+      id = res.body.data._id
+      console.log(id);
+        expect(res.statusCode).to.be.equal(201);
+        //expect(res.body.data).to.be.an('array');
+        done();
+      });
+  });
+
+
+  it('should return valid getAll note', (done) => {
+    let details = {                                                    
+      "title": "abcd",
+      "description": "aA1"
+    }
+      request(app)
+        .get('/api/v1/notes/')
+        .set('authorization', `bearer ${token}` )
+        .send(details)
+        .end((err, res) => { 
+          expect(res.statusCode).to.be.equal(202);
+          //expect(res.body.data).to.be.an('array');
+          done();
+        });
+    });
+
+
+it('should return valid get note by ID', (done) => {
+  let details = {                                                    
+    "title": "abcd",
+    "description": "aA1"
+  }
+    request(app)
+      .get(`/api/v1/notes/${id}/`)
+      .set('authorization', `bearer ${token}` )
+      .send(details)
+      .end((err, res) => {  
+        expect(res.statusCode).to.be.equal(200);
+        
+        //expect(res.body.data).to.be.an('array');
+        done();
+      });
+  });
+
+
+  it('should return valid update note by id', (done) => {
+    let details = {                                                    
+      "title": "abcd",
+      "description": "aA1"
+    }
+      request(app)
+        .put(`/api/v1/notes/${id}/`)
+        .set('authorization', `bearer ${token}` )
+        .send(details)
+        .end((err, res) => { 
+          expect(res.statusCode).to.be.equal(200);
+          //expect(res.body.data).to.be.an('array');
+          done();
+        });
+    });
+
+
+    it('should return valid archive note by id', (done) => {
+      let details = {                                                    
+        "title": "abcd",
+        "description": "aA1"
+      }
+        request(app)
+          .put(`/api/v1/notes/${id}/archive`)
+          .set('authorization', `bearer ${token}` )
+          .send(details)
+          .end((err, res) => { 
+            expect(res.statusCode).to.be.equal(202);
+            //expect(res.body.data).to.be.an('array');
+            done();
+          });
+      });
+
+
+      it('should return valid trash note by id', (done) => {
+        let details = {                                                    
+          "title": "abcd",
+          "description": "aA1"
+        }
+          request(app)
+            .put(`/api/v1/notes/${id}/trash`)
+            .set('authorization', `bearer ${token}` )
+            .send(details)
+            .end((err, res) => { 
+              expect(res.statusCode).to.be.equal(202);
+              //expect(res.body.data).to.be.an('array');
+              done();
+            });
+        });
+
+
+        it('should return valid delete note by id', (done) => {
+          let details = {                                                    
+            "title": "abcd",
+            "description": "aA1"
+          }
+            request(app)
+              .put(`/api/v1/notes/${id}/`)
+              .set('authorization', `bearer ${token}` )
+              .send(details)
+              .end((err, res) => { 
+                expect(res.statusCode).to.be.equal(200);
+                //expect(res.body.data).to.be.an('array');
+                done();
+              });
+          }); 
+});
 });
