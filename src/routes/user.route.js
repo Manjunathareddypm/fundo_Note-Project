ba58469224f70fcb141885fe136848b7da023032
mreddypm@gmail.com <mreddypm@gmail.com>
2023-04-20 11:49:16 +0530

    [Manjunatha]added:routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -10,7 +10,7 @@ const router = express.Router();
 //route to create a new user
 router.post('', newUserValidator, userController.newRegistration);
 
-//route to login
+
 router.post('/login',  userController.userLogin);
 
 
